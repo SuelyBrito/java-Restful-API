e1a3a1a8a55abf54493d634328c0e2c58a6efcd9
SuelyBrito <suelybbarbosa@hotmail.com>
2023-10-02 18:28:16 -0300

    Resolvendo desafio Padrões de Projetos

---
+++ workspace/springdemo/src/main/java/com/suely/springdemo/entities/Alunos.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.suely.springdemo.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+/**
+ *
+ * @author USUARIO
+ */
+@Entity
+@Table(name = "tb_alunos")
+public class Alunos {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @ManyToOne
+    @JoinColumn (name = "id_curso")
+    private Cursos cursos;
+
+    public Alunos() {
+    }
+
+    public Alunos(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Cursos getCursos() {
+        return cursos;
+    }
+
+    public void setCursos(Cursos cursos) {
+        this.cursos = cursos;
+    }
+  
+}
